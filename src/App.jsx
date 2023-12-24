929d7eed97045aa9390500acbd318a9ac35be63f
jahnavikandulapati <104014611+jahnavikandulapati@users.noreply.github.com>
2023-12-24 15:42:25 +0530

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jahnavi Kandulapati",
+  title: "Frontend Developer",
+  email: "jahnavikandulapati@gmail.com",
+  gitHub: "jahnavikandulapati",
+  instagram: "janukandulapati",
+  linkedIn: "jahnavikandulapati",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "jahnavi",
+  youTube: "jahnavi",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a front-end developer and also passionate about learning new things currently persuing my Bachelors degree in Shri Vishnu Engineering College for Women";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Java",
+  "Python",
+  "Machine learning",
+  "Problem Soleving",
+  "Collabration",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "The joy of mastering technology lies not just in solving problems but in the continuous pursuit of improvement and innovation.";
 
 const About = () => {
   return (
